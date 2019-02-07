c4b556ac423cd843c1abc6ecba2386248cd6b3d1
sbphillips19 <sbphillips19@gmail.com>
2019-02-06 22:13:26 -0800

    finished project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 import * as actionTypes from "./types";
 
-// USER ACTIONS
-
+/* User Actions */
 export const setUser = user => {
   return {
     type: actionTypes.SET_USER,
@@ -12,18 +11,46 @@ export const setUser = user => {
 };
 
 export const clearUser = () => {
-    return {
-        type: actionTypes.CLEAR_USER
-    }
-}
+  return {
+    type: actionTypes.CLEAR_USER
+  };
+};
 
-// CHANNEL ACTIONS
-
+/* Channel Actions */
 export const setCurrentChannel = channel => {
-    return {
-        type: actionTypes.SET_CURRENT_CHANNEL,
-        payload: {
-            currentChannel: channel
-        }
+  return {
+    type: actionTypes.SET_CURRENT_CHANNEL,
+    payload: {
+      currentChannel: channel
     }
-}
+  };
+};
+
+export const setPrivateChannel = isPrivateChannel => {
+  return {
+    type: actionTypes.SET_PRIVATE_CHANNEL,
+    payload: {
+      isPrivateChannel
+    }
+  };
+};
+
+export const setUserPosts = userPosts => {
+  return {
+    type: actionTypes.SET_USER_POSTS,
+    payload: {
+      userPosts
+    }
+  };
+};
+
+/* Colors Actions */
+export const setColors = (primaryColor, secondaryColor) => {
+  return {
+    type: actionTypes.SET_COLORS,
+    payload: {
+      primaryColor,
+      secondaryColor
+    }
+  };
+};
--- src/index.js
+++ src/index.js
@@ -51,13 +51,13 @@ class Root extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateFromProps = state => ({
   isLoading: state.user.isLoading
 });
 
 const RootWithAuth = withRouter(
   connect(
-    mapStateToProps,
+    mapStateFromProps,
     { setUser, clearUser }
   )(Root)
 );
